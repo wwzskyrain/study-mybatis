1f4d18996c8042f31a1fd0015f2589315b5eb752
erik.wang <erik.wang@ximalaya.com>
2019-12-26 22:53:49 +0800

    add generator

--- src/main/java/erik/study/mybatis/mapper/generator/dao/SysUserMapper.java
+++ src/main/java/erik/study/mybatis/mapper/generator/dao/SysUserMapper.java
@@ -1,7 +1,23 @@
 package erik.study.mybatis.mapper.generator.dao;
 
 import erik.study.mybatis.mapper.generator.domain.SysUser;
+import erik.study.mybatis.mapper.generator.domain.SysUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface SysUserMapper extends Mapper<SysUser> {
+    long countByExample(SysUserExample example);
+
+    int deleteByExample(SysUserExample example);
+
+    List<SysUser> selectByExampleWithBLOBs(SysUserExample example);
+
+    List<SysUser> selectByExample(SysUserExample example);
+
+    int updateByExampleSelective(@Param("record") SysUser record, @Param("example") SysUserExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") SysUser record, @Param("example") SysUserExample example);
+
+    int updateByExample(@Param("record") SysUser record, @Param("example") SysUserExample example);
 }
